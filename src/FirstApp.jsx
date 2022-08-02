a315a4dcd47c9e1f0aeb1e571b5ea8a5c168eef7
lautimoreyra10 <lauti_15@hotmail.es>
2022-08-02 17:25:05 -0300

    Curso React

---
+++ src/FirstApp.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ( { title, subTitle,name } ) => {
+  
+  // console.log(props);
+
+
+
+  return (
+    <>
+        <h1>{title}</h1>
+        {/* <code>{ JSON.stringify(newMessage) }</code> */}
+        <p>{subTitle}</p>
+        <p>{name}</p>
+    </>
+  )
+}
+
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+}
+FirstApp.defaultProps = {
+  title: 'No hay título',
+  subTitle: 'No hay subtítulo',
+  name: 'Lautaro Moreyra'
+}
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+// function App () {
+//     //document.createElement...
+//     return (<h1>Hola Mundo</h1>);
+// }
+
+
+// export default App;
+
+// Esta es otra forma de hacerlo, utilizando el shortcut "rafc"
+export const HelloWorldApp = () => {
+    
+  return (
+    <h1>Hello World App</h1>
+  );
+}
+
+//Otra forma mas
+//export const HelloWorldApp = () => (<h1>Arrow Function</h1>)
