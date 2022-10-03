de97fda59140978d1cb92c4484eed9f6cf75c84d
lautimoreyra10 <lauti_15@hotmail.es>
2022-10-03 13:22:51 -0300

    Pruebas con el 08, utilizando las funciones getHeroeById y getHeroesByOwner

--- tests/base-pruebas/08-08-imp-exp.test.js
+++ tests/base-pruebas/08-08-imp-exp.test.js
@@ -1,4 +1,4 @@
-import { getHeroeById } from '../../src/base-pruebas/08-imp-exp';
+import { getHeroeById, getHeroesByOwner } from '../../src/base-pruebas/08-imp-exp';
 
 describe('Pruebas en 08-imp-exp', () => { 
     test('getHeroeById debe retornar un héroe por ID', async () => { 
@@ -7,10 +7,20 @@ describe('Pruebas en 08-imp-exp', () => {
 
         expect(hero).toEqual({ id: 1, name: 'Batman', owner: 'DC' });
     });
-    test('getHeroeById debe retornar undefined si no existe el id', async () => { 
+    test('getHeroeById debe retornar undefined si no existe el id', () => { 
         const id = 100;
         const hero = getHeroeById(id);
-        console.log(hero);
+        expect( hero ).toBeFalsy();
         //expect(hero).toEqual({ id: 1, name: 'Batman', owner: 'DC' });
     });
+
+    test('Debe retornar un arreglo con los héroes de DC', () => { 
+        const owner = getHeroesByOwner('DC');
+        expect(owner.length).toEqual(3);
+     });
+     test('Debe retornar un arreglo con los héroes de Marvel', () => { 
+        const owner = getHeroesByOwner('Marvel');
+        expect(owner.length).toEqual(2);
+     });
+
  })
