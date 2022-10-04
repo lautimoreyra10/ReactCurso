75cd08c787a95c05dfcea6393a222f731e790ca6
lautimoreyra10 <lauti_15@hotmail.es>
2022-10-04 14:36:19 -0300

    Se comentó un error del catch, para posteriormente probarlo en el test.js

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -12,7 +12,7 @@ export const getImagen = async() => {
 
     } catch (error) {
         // manejo del error
-        console.error(error)
+        // console.error(error)
         return 'No se encontró la imágen';
     }
     
--- tests/base-pruebas/11-async-await.test.js
+++ tests/base-pruebas/11-async-await.test.js
@@ -1,8 +1,11 @@
 import { getImagen } from '../../src/base-pruebas/11-async-await'
 
 describe('Pruebas en 11-async-await', () => { 
-    test('getImagen debe retornar un url de la imágen', async() => { 
-        const url = await getImagen();
-        expect(typeof url).toBe('string');
-     })
- })
+    test('getImagen debe retornar un error si no tenemos api key', async() => { 
+
+        const resp = await getImagen();
+        // expect(typeof url).toBe('string');
+        expect(resp).toBe('No se encontró la imágen');
+        
+     });
+ });
